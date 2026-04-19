07bd6d88a6fae8703760fb3d5ede9769198d9593
Davud Ramazanov <davudramazanov2007@gmail.com>
2026-04-19 17:38:36 +0200

    Update Program.cs and appsettings.json

--- ClinicApplication/Program.cs
+++ ClinicApplication/Program.cs
@@ -1,3 +1,6 @@
+using ClinicApplication.Exceptions;
+using ClinicApplication.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
